6a01015b23589106e76ddf3485a455986a46e946
bigfishk <417965527@qq.com>
2019-08-30 15:41:13 +0800

    db_migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_COMMIT_TEARDOWN'] = True
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+# bootstrap = Bootstrap(app)
 
-from app import views
+from app import views, models
--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import os
+from app import app
+
 CSRF_ENABLED = True
 SECRET_KEY = 'sjflf_78DF'
 
@@ -8,3 +11,7 @@ OPENID_PROVIDERS = [
     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
